fca4ffa10384e49c46515d32c139fd57fa643455
fatemahalzahraa <fatemah.a.baqer@gmail.com>
2024-05-12 18:41:11 +0300

    solved tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cookies & Cream</h1>
+      <h3> creating cookies and more</h3>
+      <div className="Image">
+        <img
+          src="https://media.blogto.com/articles/2022225-cookies.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70"
+          alt="cookies store"
+        ></img>
+      </div>
     </div>
   );
 }
